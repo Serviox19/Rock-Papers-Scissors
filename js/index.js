7286f111293e25b5d3660f17c0e5cee5425232e9
Serviox19 <servio19@gmail.com>
2015-12-14 00:31:38 -0500

    got alerts to work but still have lots to add

--- js/index.js
+++ js/index.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+// Hide alerts from start
+  $("#sameChoice").hide()
+  $("#winAlert").hide()
+  $("#loseAlert").hide()
 
   var gameChoices = ["rock", "paper", "scissors"]
   var userSelection;
@@ -6,70 +10,70 @@ $(document).ready(function() {
   var roundCount = 1;
   var userScore = 0;
   var computerScore =0;
+  
 
   //Runs Game on the click!
 
   $("#roundCount").html(roundCount);
+  $("#userScore").html(userScore);
+  $("#computerScore").html(computerScore);
 
   $(".buttonSelection").on("click", function () {
     userSelection = $(this).attr('value');
-      console.log(userSelection);
     computerChoice = Math.floor(Math.random() * gameChoices.length);
-      console.log(computerChoice);
 
     if (computerChoice === 0 && userSelection === "rock") {
     $("#roundCount").html(roundCount = roundCount + 1);
-    alert('Same choice, Nobody Wins!');
+    $("#sameChoice").show();
   };
 
   if (computerChoice === 1 && userSelection === "paper") {
     $("#roundCount").html(roundCount = roundCount + 1);
-    alert('Same choice, Nobody Wins!');
+    $("#sameChoice").show();
   };
 
   if (computerChoice === 2 && userSelection === "scissors") {
     $("#roundCount").html(roundCount = roundCount + 1);
-    alert('Same choice, Nobody Wins!');
+    $("#sameChoice").show();
   };
 
   if (computerChoice === 0 && userSelection === "paper") {
     $("#roundCount").html(roundCount = roundCount + 1);
     $("#userScore").html(userScore = userScore + 1);
-    alert('You won!');
+    $("#winAlert").show();
   };
 
   if (computerChoice === 1 && userSelection === "scissors") {
     $("#roundCount").html(roundCount = roundCount + 1);
     $("#userScore").html(userScore = userScore + 1);
-    alert('You won!');
+    $("#winAlert").show();
   };
 
   if (computerChoice === 2 && userSelection === "rock") {
     $("#roundCount").html(roundCount = roundCount + 1);
     $("#userScore").html(userScore = userScore + 1);
-    alert('You won!');
+    $("#winAlert").show();
   };
 
   if (computerChoice == 0 && userSelection === "scissors") {
     $("#roundCount").html(roundCount = roundCount + 1);
     $("#computerScore").html(computerScore = computerScore + 1);
-    alert('Sorry Computer Won!');
+    $("#loseAlert").show();
   };
 
   if (computerChoice === 1 && userSelection === "rock") {
     $("#roundCount").html(roundCount = roundCount + 1);
     $("#computerScore").html(computerScore = computerScore + 1);
-    alert('Sorry Computer Won!');
+    $("#loseAlert").show();
   };
 
   if (computerChoice === 2 && userSelection === "paper") {
     $("#roundCount").html(roundCount = roundCount + 1);
     $("#computerScore").html(computerScore = computerScore + 1);
-    alert('Sorry Computer Won!');
+    $("#loseAlert").show();
   };
 
-  });   
+  }); 
 
-  
 
 });
