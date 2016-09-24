7696cf73487e75094569123fe9d28e7296888215
Servio Mora <servio.mora19@gmail.com>
2016-09-23 21:03:24 -0400

    new logic works, just need to display it

--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
 $(document).ready(function() {
-// Hide alerts from start
 
-  var gameChoices = ["rock", "paper", "scissors"]
+  //define variables
+
+  var gameChoices = ["rock", "paper", "scissors"];
   var userSelection;
   var computerChoice;
   var roundCount = 1;
   var userScore = 0;
   var computerScore =0;
 
-
   //Runs Game on the click!
-
-  $("#roundCount").html(roundCount);
-  $("#userScore").html(userScore);
-  $("#computerScore").html(computerScore);
 
   $(".buttonSelection").on("click", function () {
 
@@ -21,7 +17,6 @@ $(document).ready(function() {
     computerChoice = Math.floor(Math.random() * gameChoices.length);
 
     if (computerChoice === 0 && userSelection === "rock") {
-      roundCount + 1
       console.log('same choice');
     } else if (computerChoice === 1 && userSelection === "paper") {
       console.log('same choice');
@@ -29,18 +24,44 @@ $(document).ready(function() {
       console.log('same choice');
     } else if (computerChoice === 0 && userSelection === "paper") {
       console.log("You Won");
+      increaseUserScore();
     } else if (computerChoice === 1 && userSelection === "scissors") {
       console.log("You Won");
+      increaseUserScore();
     } else if (computerChoice === 2 && userSelection === "rock") {
       console.log("You Won");
+      increaseUserScore();
     } else if (computerChoice == 0 && userSelection === "scissors") {
       console.log('You Lose');
+      increaseCompScore();
     } else if (computerChoice === 1 && userSelection === "rock") {
       console.log('You Lose');
+      increaseCompScore();
     } else if (computerChoice === 2 && userSelection === "paper") {
       console.log('You Lose');
+      increaseCompScore();
     };
+      increaseRound();
+      console.log(roundCount);
+      console.log("Comp Score =" + computerScore);
+      console.log("User Score =" + userScore);
   }); // END GAME LOGIC
+
+  function increaseRound() {
+    roundCount++;
+  };
+
+  function increaseUserScore() {
+    userScore++;
+  };
+
+  function increaseCompScore() {
+    computerScore++;
+  };
+
+  $("#roundCount").html(roundCount);
+  $("#userScore").html(userScore);
+  $("#computerScore").html(computerScore);
 
   $("#reset").on("click", function() {
     $("#userScore").html(userScore = 0)
